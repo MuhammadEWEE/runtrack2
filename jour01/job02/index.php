c74ccf5265d3af422daade83b227f5fc946ecf25
muhammad.ewee <muhammad.ewee@laplateforme.io>
2024-05-22 14:34:19 +0200

    Jour01Job02

---
+++ jour01/job02/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        for($i = 0; $i < 2;$i++){// Count maximum 2 times.
+            echo "Hello La Plateforme"; /*Print the display words 
+                                        as times it count*/
+    }
+    ?>
+    
+</body>
+</html>
