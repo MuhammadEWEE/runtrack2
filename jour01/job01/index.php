1615e2890ee32ba75c3e48b01906bbb34fd07dd2
muhammad.ewee <muhammad.ewee@laplateforme.io>
2024-05-22 13:53:15 +0200

    job01

---
+++ jour01/job01/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $str = "LaPlateforme";
+        $str2 = "Vivre";
+        $str3 = "!";
+    
+        echo $str2 ,$str,$str3;
+
+        $val = 6;
+        echo $val;
+
+        $a = 4;
+        $b = $val+$a;
+        
+        echo $val," + ",$a," = ",$b;
+        echo "<br/>";
+
+        $MyBool = true;
+        echo $MyBool;
+        $MyBool = false;
+        echo $MyBool;
+    
+    ?>
+    
+</body>
+</html>
