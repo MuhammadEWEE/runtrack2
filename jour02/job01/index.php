97225b9f8e8f62f60372d23307e5a421b55a0ae3
muhammad.ewee <muhammad.ewee@laplateforme.io>
2024-05-22 16:28:36 +0200

    jour02job01

---
+++ jour02/job01/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    for ($i = 1; $i <= 1332; $i++) {
+        if ($i == 42) {
+            echo "<b><u>$i</u></b> </br>";
+        } else {
+            echo "$i </br>";
+        }
+    }
+    ?>
+    
+</body>
+</html>
