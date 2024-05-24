68377c06da878c205e9e46769755b5936ef95bd9
muhammad.ewee <muhammad.ewee@laplateforme.io>
2024-05-24 10:49:14 +0200

    jour03job03

---
+++ jour03/job03/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $str = "I'm sorry Dave I'm afraid I can't do that";
+    $vowels = "aeiouAEIOU";
+    for ($i = 0; $i < strlen($str); $i ++) {
+        if (strpos($vowels,$str[$i]) !== false){
+        echo $str[$i];
+    }
+}
+?>
+</body>
+</html>
