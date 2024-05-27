001a66b5f53d9dd32bff714768bb1b8fa7ad3807
muhammad.ewee <muhammad.ewee@laplateforme.io>
2024-05-27 14:33:28 +0200

    jour04job03

--- jour04/job03/index.php
+++ jour04/job03/index.php
@@ -3,25 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Post</title>
 </head>
 <body>
-<form action="index.php" method="get">
+<form action="index.php" method="post">
         <label for="name">Name:</label>
         <input type="text" id="name" name="name"><br><br>
+        <label for="firstname">firstname:</label>
+        <input type="text" id="firstname" name="firstname"><br><br>
         <label for="age">Age:</label>
-        <input type="text" id="age" name="age"><br><br>
-        <label for="email">Email:</label>
-        <input type="text" id="email" name="email"><br><br>
+        <input type="text" id="age" name="eage"><br><br>
         <input type="submit" value="Submit">
 </form>
 <?php
     $count = 0;
-    foreach ($_GET as $key => $value) {
+    foreach ($_POST as $key => $value) {
         $count++;
     }
 
-    echo "The number of GET arguments sent is: " . $count;
+    echo "The number of POST arguments sent is: " . $count;
     ?>
 
 
