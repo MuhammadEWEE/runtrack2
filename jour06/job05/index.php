df513ee202b1d5ac40b4972cb9c6a023d02f2f0f
muhammad.ewee <muhammad.ewee@laplateforme.io>
2024-05-29 14:10:29 +0200

    jour07job01

--- jour06/job05/index.php
+++ jour06/job05/index.php
@@ -4,23 +4,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dropdown 3 Style</title>
-</head>
-<body>
     <?php
     $selected_style ='';
+    
     if (isset($_POST['style'])){
-        $selected_style = ($_POST['style']);
+        $selected_style = htmlspecialchars($_POST['style']);
         echo '<link rel="stylesheet" href="' . $selected_style . '.css">';
     }
     ?>
+</head>
+<body>
+  
     <form method="post" action="">
         <label for="style">Logo apple evolution</label>
         <select name="style" id="style">
             <option value="style1"<?php if ($selected_style == '$style1') echo 'selected'; ?>>1976</option>
-            <option value="style2"<?php if ($selected_style == '$style1') echo 'selected'; ?>>1977-1998</option>
-            <option value="style3"<?php if ($selected_style == '$style1') echo 'selected'; ?>>1998-present</option>
+            <option value="style2"<?php if ($selected_style == '$style2') echo 'selected'; ?>>1977-1998</option>
+            <option value="style3"<?php if ($selected_style == '$style3') echo 'selected'; ?>>1998-present</option>
         </select>
-        <input type="submit" value="apply">
+        <input type="submit" value="submit">
     </form>
 </body>
 </html>
