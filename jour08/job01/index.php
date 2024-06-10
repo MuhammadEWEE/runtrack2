40578b55b87983091c0580c2c6dc852220e83894
muhammad.ewee <muhammad.ewee@laplateforme.io>
2024-06-10 11:02:37 +0200

    jour08job01

---
+++ jour08/job01/index.php
@@ -0,0 +1,30 @@
+<?php
+session_start();
+
+if(!isset($_SESSION['nbvisits'])){
+$_SESSION['nbvisits']=0;
+}
+if(isset($_POST['reset'])){
+$_SESSION['nbvisits']=0;
+}else{
+    $_SESSION['nbvisits']++;
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Count Number Of Visiters</h1>
+    <p>Visit Time <?php echo $_SESSION['nbvisits']; ?></p>
+    <form methode="post">
+        <input type="submit" name="reset" value="Reset">
+    </form>
+</body>
+</html>
